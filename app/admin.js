9ccdd5196c8fd22058d863c36118db32d12e255b
Jeremy Bohannon <jeremybohannon@outlook.com>
2016-04-05 05:29:12 +0000

    Added admin verification

--- app/admin.js
+++ app/admin.js
@@ -1,11 +1,36 @@
 window.addEventListener('load', function () {
 
+  var account = prompt("Please enter your account name: ");
+  var password = prompt("Please enter your password: ");
   var fb = new Firebase("makegaminggreat.firebaseio.com/");
 
-
-  btnReset.style.cursor = "pointer";
-
-  btnReset.onclick = function() {
-    fb.set({});
-  };    
+  if(account && password)
+  {
+    fb.once("value", function(snapshot){
+      if(snapshot.child(account).exists)
+      {
+        if(snapshot.child(account).val() == password)
+        {
+          
+          var fb2 = new Firebase("makegaminggreat.firebaseio.com/users/");
+          
+          var reset = document.getElementById("btnReset");
+          reset.style.visibility = "visible";
+          
+          console.log('test');
+          
+          
+          btnReset.style.cursor = "pointer";
+          
+          btnReset.onclick = function() 
+          {
+            fb2.set({});
+          };
+        
+        }
+      }
+    }); 
+  };
 });
+      
+      
